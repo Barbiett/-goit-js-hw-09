dc138508bb7d6d9f73c2e271b5d57580e462d465
Barbiett <146207003+Barbiett@users.noreply.github.com>
2024-03-11 11:19:47 +0000

    Deploying to gh-pages from @ Barbiett/-goit-js-hw-09@484c0acb2eb3a218876b2d096bfc93342570b28c 🚀

---
+++ assets/2-form-bcaa0e74.js
@@ -0,0 +1,2 @@
+const a="feedback-form-state",e=document.querySelector(".feedback-form"),t=JSON.parse(localStorage.getItem(a)),s=e.elements.message,n=e.elements.email;t&&(s.value=t.message,n.value=t.email);function c(o){const l=s.value.trim(),m=n.value.trim(),r=JSON.stringify({email:m,message:l});localStorage.setItem(a,r)}function i(o){o.preventDefault(),console.log({message:s.value,email:n.value}),localStorage.removeItem(a),e.reset()}e.addEventListener("input",c);e.addEventListener("submit",i);
+//# sourceMappingURL=2-form-bcaa0e74.js.map
